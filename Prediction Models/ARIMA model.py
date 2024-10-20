eda52a945def67d8e9e100b10b6a816ed95fee34
Anouar Saidan <133757577+Asaidan@users.noreply.github.com>
2024-10-20 15:26:03 +0200

    Update ARIMA model.py

--- Prediction Models/ARIMA model.py
+++ Prediction Models/ARIMA model.py
@@ -27,7 +27,7 @@
 import configparser
 
 config = configparser.ConfigParser()
-files_read = config.read('/Workspace/Users/anouar.saidan@sonepar.com/anouar.saidan@sonepar.com/config.ini')
+files_read = config.read('/Workspace/.../config.ini')
 country = config['parameters']['country']
 print(f"Country: {country}")
 
